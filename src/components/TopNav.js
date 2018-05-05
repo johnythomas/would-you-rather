7b4f254df8477b99e8641bbfb652c99905cd5f1e
johnythomas <johnythomas007@gmail.com>
2018-05-05 13:05:28 +0530

    Feat: Show name in nav
    * Show name in navbar
    * Move logout button to a dropdown
    * Add link to new profile screen

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,67 +1,133 @@
-import React from "react"
+import React, { Component, Fragment } from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
+import { Link } from "react-router-dom"
 import {
   AppBar,
   Toolbar,
   IconButton,
   Typography,
-  Button,
-  Avatar
+  Avatar,
+  withStyles
 } from "material-ui"
+import Menu, { MenuItem } from "material-ui/Menu"
 import MenuIcon from "@material-ui/icons/Menu"
+import { ArrowDropDown } from "@material-ui/icons"
 import { logout } from "../actions/authedUser"
 import { toggleDrawer } from "../actions/drawer"
 
 const styles = {
-  iconButton: {
-    marginLeft: -12,
-    marginRight: 20
-  },
-  flex: {
-    flex: 1
-  },
   profilePic: {
-    marginRight: 5
+    marginLeft: 5
+  },
+  pageTitleLink: {
+    textDecoration: "none",
+    flex: 1,
+    color: "white"
+  },
+  name: {
+    textTransform: "uppercase"
+  },
+  arrowdropDown: {
+    width: "14px"
   }
 }
 
-const TopNav = ({ toggleDrawerState, user, doLogout }) => (
-  <AppBar position="static">
-    <Toolbar>
-      {user && (
-        <IconButton
-          onClick={() => toggleDrawerState()}
-          style={styles.iconButton}
-          color="inherit"
-          aria-label="Menu"
-        >
-          <MenuIcon />
-        </IconButton>
-      )}
-      <Typography variant="title" color="inherit" style={styles.flex}>
-        Would You Rather
-      </Typography>
-      {user && (
-        <Button color="inherit" onClick={() => doLogout()}>
-          <Avatar
-            aria-label="Recipe"
-            style={styles.profilePic}
-            src={user.avatarURL}
-          />
-          Logout
-        </Button>
-      )}
-    </Toolbar>
-  </AppBar>
-)
+class TopNav extends Component {
+  state = {
+    anchorEl: null
+  }
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget })
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null })
+  }
+
+  render() {
+    const { classes, toggleDrawerState, user, doLogout } = this.props
+    const { anchorEl } = this.state
+    const open = Boolean(anchorEl)
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          {user && (
+            <IconButton
+              onClick={() => toggleDrawerState()}
+              className={classes.iconButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+          <Link to="/" className={classes.pageTitleLink}>
+            <Typography variant="title" color="inherit">
+              Would You Rather
+            </Typography>
+          </Link>
+          {user && (
+            <Fragment>
+              <Typography
+                className={classes.name}
+                color="inherit"
+                variant="subheading"
+              >
+                {user.name}
+              </Typography>
+              <Avatar
+                aria-label="Recipe"
+                className={classes.profilePic}
+                src={user.avatarURL}
+              />
+              <div>
+                <IconButton
+                  className={classes.arrowdropDown}
+                  aria-owns={open ? "menu-appbar" : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <ArrowDropDown />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={this.state.anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={doLogout}>Logout</MenuItem>
+                </Menu>
+              </div>
+            </Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
 
 TopNav.propTypes = {
   toggleDrawerState: PropTypes.func.isRequired,
   doLogout: PropTypes.func.isRequired,
   user: PropTypes.shape({
     avatarURL: PropTypes.string.isRequired
-  })
+  }),
+  classes: PropTypes.shape({
+    profilePic: PropTypes.string.isRequired,
+    pageTitleLink: PropTypes.string.isRequired
+  }).isRequired
 }
 
 TopNav.defaultProps = {
@@ -75,4 +141,4 @@ const mapStateToProps = ({ authedUser, users }) => ({
 export default connect(mapStateToProps, {
   doLogout: logout,
   toggleDrawerState: toggleDrawer
-})(TopNav)
+})(withStyles(styles)(TopNav))
